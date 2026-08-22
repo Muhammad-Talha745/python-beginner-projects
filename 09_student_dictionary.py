644bee74ca3c08238bd0ec1be0b05a1a58845ce7
Muhammad Talha Iftikhar <raotalhaop@gmail.com>
2026-08-22 19:31:42 +0500

    Add dictionary project

---
+++ 09_student_dictionary.py
@@ -0,0 +1,21 @@
+name = input("Enter your name:")
+age = int(input("Enter your age:"))
+fav_sub = input("Enter you favorite subject:")
+
+name = name.capitalize()
+fav_sub = fav_sub.capitalize()
+
+student = { 'name' : name,  'age' : age, 'fav_sub' : fav_sub }
+
+print(student)
+print(student["name"])
+
+student["age"] = age + 1
+
+country = input("Enter your country name:")
+
+country = country.capitalize()
+
+student["country"] = country
+
+print(student)
