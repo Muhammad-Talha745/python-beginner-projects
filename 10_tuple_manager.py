079bf673abda98491b1d8fe19796a22635710657
Muhammad Talha <raotalhaop@gmail.com>
2026-09-05 13:52:58 +0500

    Add tuple manager project

---
+++ 10_tuple_manager.py
@@ -0,0 +1,11 @@
+fav_foods = ("Burger", "Pizza", "Maggie", "Pasta", "Fries")
+
+print(fav_foods)
+print(fav_foods[0])
+print(fav_foods[4])
+print(fav_foods[0:3])
+
+fav_foods2 = ("Chicken", "Beef", "Mutton")
+final_foods = fav_foods + fav_foods2
+
+print(final_foods)
