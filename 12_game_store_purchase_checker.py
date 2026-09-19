df48b5e558c1667110de50c78abf6c0551b10371
Muhammad Talha <raotalhaop@gmail.com>
2026-09-19 15:41:17 +0500

    Add purchase status checker

--- 12_game_store_purchase_checker.py
+++ 12_game_store_purchase_checker.py
@@ -1,7 +1,11 @@
 name = input("Customer Name: ").capitalize()
+
 game = input("Game Name: ").capitalize()
+
 price = int(input("Game price: "))
+
 ava_bal = int(input("Available balance: "))
+
 membership = input("Membership? (yes/no): ").lower()
 
 discount = 0
@@ -14,20 +18,29 @@ print(f"Membership: {membership}")
 
 if membership == "yes":
     print("20% Discount")
+
     discount = price * 20 / 100
     final_price = price - discount
+
 else:
     print("No Discount")
+
     final_price = price
 
 if ava_bal >= final_price:
-    print("Purchase Successful")
+    purchase_status = "Purchase Successful"
     remaining_balance = ava_bal - final_price
+
+elif ava_bal >= final_price - 5000:
+    purchase_status = "Insufficient Balance"
+    remaining_balance = ava_bal
+
 else:
-    print("Cannot afford the game")
+    purchase_status = "Balance Too Low"
     remaining_balance = ava_bal
 
 print(f"Game: {game}")
 print(f"Discount: {discount}")
 print(f"Final Price: {final_price}")
 print(f"Remaining Balance: {remaining_balance}")
+print(f"Purchase Status: {purchase_status}")
