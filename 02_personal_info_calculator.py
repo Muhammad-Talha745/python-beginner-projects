3cf8cbadf7432e251cae0dfaf53991aab5bb80a5
Muhammad Talha <raotalhaop@gmail.com>
2026-09-19 15:38:10 +0500

    Add favorite number checker

--- 02_personal_info_calculator.py
+++ 02_personal_info_calculator.py
@@ -1,17 +1,29 @@
 name = input("Enter your full name: ")
+
 age = int(input("Enter your age: "))
+
 fav_num = int(input("Enter your favourite number: "))
 
+
 name = name.upper()
+
 
 is_name = name.isdigit()
 
 age_5 = age + 5
 
 fav_num_half = fav_num / 2
+
 fav_num_floor_half = fav_num // 2
 
 age_fav_num = age >= 18 and fav_num > 10
+
+
+if fav_num % 2 == 0:
+    number_type = "Even"
+else:
+    number_type = "Odd"
+
 
 print(
     f"My name is {name} (is all digits: {is_name}), I am {age} years old, "
@@ -21,3 +33,5 @@ print(
 )
 
 print(f"Age is 18 or older and favourite number is greater than 10: {age_fav_num}")
+
+print(f"My favourite number is {number_type}.")
