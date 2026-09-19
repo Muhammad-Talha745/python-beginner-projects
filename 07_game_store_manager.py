aa7617af354f6f9d76cef3c5293b7df67ab37769
Muhammad Talha <raotalhaop@gmail.com>
2026-09-19 15:40:00 +0500

    Add expensive game counter

--- 07_game_store_manager.py
+++ 07_game_store_manager.py
@@ -1,7 +1,9 @@
 name = input("Enter your name: ")
+
 game1 = input("Enter the name of first game: ")
 game2 = input("Enter the name of second game: ")
 game3 = input("Enter the name of third game: ")
+
 price1 = int(input("Enter the price of first game: "))
 price2 = int(input("Enter the price of second game: "))
 price3 = int(input("Enter the price of third game: "))
@@ -20,9 +22,11 @@ avg = total / 3
 print("Customer Name:", name)
 print("GAME STORE")
 print("Games:")
+
 print(game1, "Rs.", price1)
 print(game2, "Rs.", price2)
 print(game3, "Rs.", price3)
+
 print("Total:", total)
 print("Average:", avg)
 print("Most Expensive:", max(prices))
@@ -32,18 +36,26 @@ new_game = input("Enter the new game name: ")
 new_price = int(input("Enter the price of new game: "))
 
 new_game = new_game.capitalize()
+
 games.append(new_game)
 prices.append(new_price)
 
-remove_games = input("Enter the game name that you want to remove: ")
+remove_game = input("Enter the game name that you want to remove: ")
+remove_game = remove_game.capitalize()
 
-remove_games = remove_games.capitalize()
-remove_price = games.index(remove_games)
-games.remove(remove_games)
+remove_price = games.index(remove_game)
+
+games.remove(remove_game)
 prices.remove(prices[remove_price])
 
 total = prices[0] + prices[1] + prices[2]
 avg = total / 3
+
+expensive_games = 0
+
+for price in prices:
+    if price > 30000:
+        expensive_games = expensive_games + 1
 
 print("Updated Games:", games)
 print("Updated Prices:", prices)
@@ -51,3 +63,4 @@ print("Updated Total:", total)
 print("Updated Average:", avg)
 print("Updated Most Expensive:", max(prices))
 print("Updated Cheapest:", min(prices))
+print("Games Above Rs. 30,000:", expensive_games)
