cfc818d0f29fd291e76f6d1e3dbac56835613c0c
Muhammad Talha <raotalhaop@gmail.com>
2026-09-19 15:40:35 +0500

    Add student age status

--- 09_student_dictionary.py
+++ 09_student_dictionary.py
@@ -1,21 +1,29 @@
-name = input("Enter your name:")
-age = int(input("Enter your age:"))
-fav_sub = input("Enter your favorite subject:")
+name = input("Enter your name: ")
+
+age = int(input("Enter your age: "))
+
+fav_sub = input("Enter your favorite subject: ")
 
 name = name.capitalize()
 fav_sub = fav_sub.capitalize()
 
-student = { 'name' : name,  'age' : age, 'fav_sub' : fav_sub }
+student = {"name": name, "age": age, "fav_sub": fav_sub}
 
 print(student)
+
 print(student["name"])
 
 student["age"] = age + 1
 
-country = input("Enter your country name:")
+country = input("Enter your country name: ")
 
 country = country.capitalize()
 
 student["country"] = country
+
+if student["age"] >= 18:
+    student["status"] = "Adult"
+else:
+    student["status"] = "Under 18"
 
 print(student)
