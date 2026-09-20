6c5ed724e7293ed65d736e09abdc430fd00bd860
Muhammad Talha <raotalhaop@gmail.com>
2026-09-20 14:29:43 +0500

    Improve student age status

--- 09_student_dictionary.py
+++ 09_student_dictionary.py
@@ -1,7 +1,5 @@
 name = input("Enter your name: ")
-
 age = int(input("Enter your age: "))
-
 fav_sub = input("Enter your favorite subject: ")
 
 name = name.capitalize()
@@ -10,7 +8,6 @@ fav_sub = fav_sub.capitalize()
 student = {"name": name, "age": age, "fav_sub": fav_sub}
 
 print(student)
-
 print(student["name"])
 
 student["age"] = age + 1
