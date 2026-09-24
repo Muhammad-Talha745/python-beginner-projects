ee6d8cc9e6c6a5c83608153607c8bd98d9b4a396
Muhammad Talha <raotalhaop@gmail.com>
2026-09-24 12:35:05 +0500

    Improve tuple food manager

--- 10_tuple_manager.py
+++ 10_tuple_manager.py
@@ -1,15 +1,31 @@
-fav_foods = ("Burger", "Pizza", "Maggie", "Pasta", "Fries")
+food1 = input("Enter your first favorite food: ").title()
+food2 = input("Enter your second favorite food: ").title()
+food3 = input("Enter your third favorite food: ").title()
+food4 = input("Enter your fourth favorite food: ").title()
+food5 = input("Enter your fifth favorite food: ").title()
 
-print(fav_foods)
-print(fav_foods[0])
-print(fav_foods[4])
-print(fav_foods[0:3])
+fav_foods = (food1, food2, food3, food4, food5)
 
-fav_foods2 = ("Chicken", "Beef", "Mutton")
+print("Favorite Foods:", fav_foods)
+print("First Food:", fav_foods[0])
+print("Last Food:", fav_foods[4])
+print("First Three Foods:", fav_foods[0:3])
+
+food1 = input("Enter another food: ").title()
+food2 = input("Enter another food: ").title()
+food3 = input("Enter another food: ").title()
+
+fav_foods2 = (food1, food2, food3)
 
 final_foods = fav_foods + fav_foods2
 
+print("\nFinal Foods:", final_foods)
 
-print(final_foods)
-print("Pizza Count:", final_foods.count("Pizza"))
-print("Pizza Position:", final_foods.index("Pizza"))
+search_food = input("\nEnter a food to search: ").title()
+
+print("Food Count:", final_foods.count(search_food))
+
+if search_food in final_foods:
+    print("Food Position:", final_foods.index(search_food))
+else:
+    print("Food not found.")
